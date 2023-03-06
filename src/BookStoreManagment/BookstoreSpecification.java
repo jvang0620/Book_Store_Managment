87953a10885051443406877e37843be91ef859ef
Jai_Vang <jaivang0387@gmail.com>
2023-03-06 15:02:36 -0500

    first commit

---
+++ src/BookStoreManagment/BookstoreSpecification.java
@@ -0,0 +1,29 @@
+
+package BookStoreManagment;
+
+/**
+ *
+ * @author Jai Vang
+ */
+public interface BookstoreSpecification {
+    
+    /**
+     * given a product id and a product quantity, update
+     * stock by adding amount to existing product quantity
+     * 
+     * @param productId
+     * @param amount
+     * @return 
+     */
+    public int restockProduct(int productId, int amount);
+    
+    /**
+     * calculate and return dollar amount for current inventory of products
+     * that is in stock
+     * @return total in stock value
+     */
+    public double inventoryValue();
+    
+    
+    
+} //end of class
